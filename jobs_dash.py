e82b0507bd2147ab77c9dbe03575a5fc98834aca
Bryan Swagerty <bryantee@gmail.com>
2015-01-27 09:37:15 -0700

    First real commit
    Fixed test print line to print job name and address.

--- jobs_dash.py
+++ jobs_dash.py
@@ -2,4 +2,4 @@ import jobs
 
 fake_street = jobs.Jobs("123 fake st", "Phoenix", "AZ", "85257")
 
-print(fake_street.job_name), ": ", fake_street.address 
+print(fake_street.job_name, ": ", fake_street.address) 
