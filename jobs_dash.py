f78fc0b0ffaecb672c9f94a9f6a3ccdc8276e737
Bryan Swagerty <bryantee@gmail.com>
2015-02-11 10:41:11 -0700

    Cleanup

--- jobs_dash.py
+++ jobs_dash.py
@@ -5,6 +5,7 @@ import jobs
 fake_street = jobs.Jobs("123 fake st Phoenix, AZ 85255", "2-1-15")
 fake_street2 = jobs.Jobs("5496 Yabba-dabba-do st", "2-4-15")
 fake_street3 = jobs.Jobs("9999 yeah yeah yeahs", "2-17-15")
+fake_street4 = jobs.Jobs("blah blah blah", "2-22-15")
 
 
 # Adding some phases to the jobs for testing
@@ -24,13 +25,14 @@ def showOpenJobs():
 	for job in jobs.Jobs.open_jobs:
 		print(job.address + ' is due ' + job.due)
 		for i in job.phase:
-			print(' - %s:' % i)
-			print(job.phase[i] + '\n')
+			print(' - ', i, ': ', job.phase[i], '\n')
 
+# test out showing number of open jobs
 def openJobCount():
 	return len(jobs.Jobs.open_jobs)
 
-openJobCount()
+print('Number of open jobs: ', openJobCount())
+
 
 
 
